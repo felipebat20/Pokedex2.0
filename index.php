2e82d567382e4d40586e245d77bafa68f6ab2bd0
Felipe <felipebatista24680@gmail.com>
2019-12-20 10:36:55 -0300

    Mudancas na interface

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ require_once 'funcoes.php'; ?>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pokedex Testando</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="_css/estilo.css">
     <div>Font made from <a href="http://www.onlinewebfonts.com">oNline Web Fonts</a>is licensed by CC BY 3.0</div>
 
@@ -29,30 +30,31 @@ require_once 'funcoes.php'; ?>
                         <label for="search">Nome ou número</label>
                         <div class="emlinha">
                             <input type="text" name="search" placeholder="Procure pelo nome ou ID" class="form-control">
-                            <button type="submit" name="submit-search" class=""><img src="./_images/search-solid.svg" alt=""></button>
+                            <button type="submit" name="submit-search" class="fa fa-search" id="btn-search"></button>
                         </div>
-                        <?php
-                        if (is_null($result)) {
-                            $result = $mysqli->query("SELECT  * FROM pokemon LIMIT 0, 10");
-                        }
-                        ?>
                     </div>
             </div>
         </div>
         <div id="bloco">
+            <!-- <form action="index.php" method="get">
+                <input type="hidden" name="id">
+                <input type="submit" >
+            </form> -->
 
             <div class="cartoes">
                 <?php while ($row = $result->fetch_assoc()) : ?>
-                <div class="cartao">
-                    <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/<?php formatid($row['id']) ?>.png" alt="avatar">
-                    <div class="info">
-                        <h6><b>Nº<?php formatid($row['id']); ?></b></h6>
-                        <h4><b><?php echo $row['name']; ?></b></h4>
-                        <p>
-                            <?php tipo1($row['type1'], $row['type2']); ?>
-                        </p>
-                    </div>
-                </div>
+                    
+                        <div class="cartao">
+                            <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/<?php formatid($row['id']) ?>.png" alt="avatar">
+                            <div class="info">
+                                <h6><b>Nº<?php formatid($row['id']); ?></b></h6>
+                                <h4><b><?php echo $row['name']; ?></b></h4>
+                                <p>
+                                    <?php tipo1($row['type1'], $row['type2']); ?>
+                                </p>
+                            </div>
+                        </div>
+                    
 
                 <?php endwhile; ?>
             </div>
--- listar_pokemon.php
+++ listar_pokemon.php
@@ -1,22 +1,16 @@
 <?php require_once 'conexao.php';
-//consultar no banco de dados
-$pesquisa = false;  
+
+ 
 if (isset($_POST['submit-search'])) {
     $search = $_POST['search'];
-    $result = $mysqli->query("SELECT  * FROM pokemon WHERE id='$search'");
-    $pesquisa = true;           
+    $result = $mysqli->query("SELECT  * FROM pokemon WHERE id='$search' OR name LIKE '%".$search."%' ");
+               
 }else{
 $result = $mysqli->query("SELECT  * FROM pokemon LIMIT 0, 12");
 }
 
 
-//Efetuar a busca
-// if (isset($_GET['submit-search'])) {
-//     $search = $_GET['search'];
-//     $result = $mysqli->query("SELECT  * FROM pokemon WHERE id='$search'");  
-//     $pesquisa = true;
-//     header('location: index.php');
-// }
+
 
 
 ?>
